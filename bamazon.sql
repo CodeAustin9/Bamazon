47787048f68a277261fe11f691b67619c7af560b
Code Austin 9 <630smileaa@gmail.com>
2018-05-31 09:37:50 -0600

    update

---
+++ bamazon.sql
@@ -0,0 +1,32 @@
+-- DROP DATABASE IF EXISTS bamazon_db;
+
+-- CREATE DATABASE bamazon_db;
+
+-- USE bamazon_db;
+
+-- CREATE TABLE products (
+--   item_id INTEGER(10) NOT NULL AUTO_INCREMENT, 
+--   product_name VARCHAR(255) NULL,
+--   skill_level VARCHAR(255) NULL,
+--   department_name VARCHAR(255) NULL,
+--   price DECIMAL(10,2) NULL,
+--   stock_quantity INT NOT NULL,
+--   PRIMARY KEY (item_id)
+-- );
+
+-- INSERT INTO products (item_id, product_name, skill_level, department_name, price, stock_quantity)
+-- VALUES (1, "first rental", "basic", "plant", 19.99, 1000), 
+-- (2, "maddness", "intermediate", "plant", 19.99, 1000),
+-- (3, "first eko", "entry level fields", "plant", 19.99, 1000),
+-- (4, "maddness zero", "performance", "plant", 19.99, 1000),
+-- (5, "first hard", "entry level", "plant",  19.99, 1000),
+-- (6, "demmented", "tournament", "plant",  19.99, 1000),
+-- (7, "#", "beginner mobile", "plant",  19.99, 1000),
+-- (8, "#", "intermediate mobile", "plant",  19.99, 1000),
+-- (9, "#", "advanced mobile", "plant",  19.99, 1000),
+-- (10, "inzanity", "advanced accurate", "plant",  19.99, 1000);
+
+
+-- SELECT * FROM products;
+
+
